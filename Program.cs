5ad431cf442e1bf953c80d7f2b9e9390fc8065a8
UgurSerper <serefaslan5656@gmail.com>
2026-07-20 22:39:49 +0300

    KullaniciEkle methodu yazildi anamenu yazildi kullanici nesnesi duzeltildi

--- Program.cs
+++ Program.cs
@@ -1,1 +1,45 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace Banka
+{
+    public class Program
+    {
+        public void AnaMenu()
+        {
+            KullaniciServices ks = new KullaniciServices();
+            int secim = 0;
+
+            while (true)
+            {
+                Console.WriteLine("===BANKA UYGULAMASI===");
+                Console.WriteLine("1 - GIRIS YAP");
+                Console.WriteLine("2 - HESAP OLUSTUR");
+                Console.WriteLine("0 - CIKIS");
+                Console.Write("Seciminiz");
+
+                if(!int.TryParse(Console.ReadLine(), out secim))
+                {
+                    Console.WriteLine("Lutfen gecerli bir sayi giriniz.");
+                    continue;
+                }
+
+                switch (secim)
+                {
+                    case 1: ks.GirisYap(); break;
+                    
+                    case 2: ks.KullaniciEkle(); break;
+
+                    case 0: return;
+
+                        default: Console.WriteLine("Lutfen gecerli bir secim giriniz."); break;
+                }
+            }
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            p.AnaMenu();
+        }
+    }
+}
--- Services/KullaniciServices.cs
+++ Services/KullaniciServices.cs
@@ -4,6 +4,17 @@ namespace Banka
     {
         private List<Kullanici> kullanicilar = new List<Kullanici>();
 
+        public KullaniciServices() 
+        { 
+            Kullanici musteri = new Kullanici("Ugur","Aslan",1,"5656aslan",1000.00m,KullaniciRolu.Musteri, new DateTime(2025,1,1));
+            Kullanici musteri2 = new Kullanici("Ibrahim","Aslan",2,"5656aslan",1000.00m,KullaniciRolu.Musteri, new DateTime(2024,1,1));
+            Kullanici musteri3 = new Kullanici("Musa","Aslan",3,"5656aslan",1000.00m,KullaniciRolu.Musteri, new DateTime(2023,1,1));
+
+            kullanicilar.Add(musteri);
+            kullanicilar.Add(musteri2);
+            kullanicilar.Add(musteri3);
+        }
+
         public void KullaniciEkle()
         {
             
