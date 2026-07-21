8e3e9dceb95621478268a45974205c16c3b357ca
UgurSerper <serefaslan5656@gmail.com>
2026-07-21 09:39:10 +0300

    para yaıtr ve para cek methodları eklendi*

--- Services/BankaServices.cs
+++ Services/BankaServices.cs
@@ -2,13 +2,52 @@ namespace Banka
 {
     public class BankaServices
     {
-        public void ParaYatir()
+        public void ParaYatir(Kullanici aktifKullanici)
         {
-            
+            decimal yatirilacakBakiye =0;
+            Console.Write("Yatırılacak Bakiye : ");
+            if(!decimal .TryParse(Console.ReadLine(),out yatirilacakBakiye))
+            {
+                Console.WriteLine("Geçerli bir sayı giriniz.");
+                return;
+            }
+            if(yatirilacakBakiye< 0)
+            {
+                Console.WriteLine("Yatırılacak bakiye negatif olmaz.");
+                return;
+            }
+
+            aktifKullanici.Bakiye+=yatirilacakBakiye;
+
+            Console.WriteLine($"{yatirilacakBakiye} TL başarıyla çekildi.");
+            Console.WriteLine($"Güncel Bakiyeniz : {aktifKullanici.Bakiye} TL");
+
         }
-        public void ParaCek()
+        public void ParaCek(Kullanici aktifKullanici)
         {
-            
+            decimal cekilecekBakiye =0;
+            Console.Write("Cekilecek Bakiye : ");
+            if(!decimal .TryParse(Console.ReadLine(),out cekilecekBakiye))
+            {
+                Console.WriteLine("Geçerli bir sayı giriniz.");
+                return;
+            }
+            if(cekilecekBakiye<= 0)
+            {
+                Console.WriteLine("Çekilecek bakiye 0dan büyük olmalı.");
+                return;
+            }
+            if(cekilecekBakiye> aktifKullanici.Bakiye)
+            {
+                Console.WriteLine("Yetersiz Bakiye");
+                return;
+            }   
+            aktifKullanici.Bakiye-=cekilecekBakiye;
+
+            Console.WriteLine($"{cekilecekBakiye} TL başarıyla çekildi.");
+            Console.WriteLine($"Güncel Bakiyeniz : {aktifKullanici.Bakiye} TL");
+
+
         }
         public void Havale()
         {
